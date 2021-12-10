ea4ab73f141fa239556ee17d99ec0fd0013fc6f6
wanhuaying.navinfo.com <wanhuaying@navinfo.com>
2021-12-10 17:36:12 +0800

    add files

--- MainApp/MainApp.go
+++ MainApp/MainApp.go
@@ -209,14 +209,16 @@ func (me *MainApp) SimpleCheckParameters() {
 	}
 
 	help := false
-	// env := "test"
-	// for _, para := range me.Paras {
-	// 	if para.Name == "h" {
-	// 		help = *(para.Value.(*bool))
-	// 	} else if para.Name == "e" {
-	// 		env = *(para.Value.(*string))
-	// 	}
-	// }
+	env := "test"
+	for _, para := range me.Paras {
+		if para.Name == "h" {
+			help = *(para.Value.(*bool))
+		} else if para.Name == "env" {
+			env = *(para.Value.(*string))
+		}
+	}
+
+	log.Printf("Run Environment:[%v]", env)
 
 	if help {
 		me.PrintUsage()
